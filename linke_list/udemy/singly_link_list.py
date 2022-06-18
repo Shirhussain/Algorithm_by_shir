fdf38e2e997d1330c27eeacc41f2e6e1e3e50eb9
Shir Danishyar <sh.danishyar@gmail.com>
2022-06-18 18:22:42 -0500

    add deletetion in SLL

--- linke_list/udemy/singly_link_list.py
+++ linke_list/udemy/singly_link_list.py
@@ -48,6 +48,9 @@
 # singleLL.insertSLL(0,3)
 # print([node.value for node in singleLL])
 
+from re import T
+
+
 class Node:
     def __init__(self, value=None):
         self.value = value
@@ -88,12 +91,91 @@ class SinglyLinkedList:
                 new_node.next = next_node
                 if temp_node == new_node:
                     self.tail = new_node
+                    
+                    
+    def traverseSLL(self):
+        # traverse throw the linklist
+        if self.head is None:
+            print("Ths singlyLinkList does not exist")
+        else:
+            node = self.head
+            while node is not None:
+                print(node.value)
+                node = node.next
+    
+    def searchSLL(self, node_value):
+        if self.head is None:
+            return "The singlyLinkList does not exist"
+        else:
+            node = self.head
+            while node is not None:
+                if node.value == node_value:
+                    return node.value
+                node = node.next
+            return "The value dose not exist in the lsit"  
+        
+    def delete_node(self, location):
+        if self.head is None:
+            print("The singlyLinkList does not exist")
+        else:
+            if location == 0:
+                # check if the list have only one element, it means that head and tail are one node
+                if self.head == self.tail:
+                    self.head = None
+                    self.tail = None
+                else:
+                    # it means that delte first node and connect to the next node
+                    self.head = self.head.next
+            elif location == -1:
+                if self.head == self.tail:
+                    self.head = None
+                    self.tail = None
+                else:
+                    # otherwise we have to traverse the list tail reach the end
+                    node = self.head
+                    while node is not None:
+                        if node.next == self.tail:
+                            break
+                        node = node.next
+                    node.next = None
+                    self.tail = node
+            
+            else:
+                temp_node = self.head
+                index = 0
+                while index < location -1:
+                    temp_node = temp_node.next
+                    index += 1
+                next_node = temp_node.next
+                temp_node.next = next_node.next
+                
+    def deleteEntireSLL(self):
+        if self.head is None:
+            print("The link does not exist")
+        else:
+            # all you need to do to delte entire link is to assign head and teail to None
+            # the garbage collector will delete nodes
+            self.head = None
+            self.tail = None
+            
 
 sll = SinglyLinkedList()
 sll.insert(1,1)
 sll.insert(2,1)
 sll.insert(3,1)
 sll.insert(10,2)
+sll.insert(11,2)
+sll.insert(100,2)
 
 
 print([node.value for node in sll])
+
+# sll.traverseSLL()
+# print(sll.searchSLL(10))
+# sll.delete_node(2)
+
+sll.deleteEntireSLL()
+
+print([node.value for node in sll])
+
+
