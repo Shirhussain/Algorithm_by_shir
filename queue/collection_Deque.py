c7b2e976c72b83b9b4475d4e3ba278aeeee08623
Shir Danishyar <sh.danishyar@gmail.com>
2022-06-28 22:42:38 -0500

    collections deque added

---
+++ queue/collection_Deque.py
@@ -0,0 +1,16 @@
+from calendar import c
+from collections import deque
+
+# deque works like cercular queue, with max capacity
+custome = deque(maxlen=4)
+custome.append(1)
+custome.append(2)
+custome.append(3)
+custome.append(4)
+custome.append(5)
+
+
+custome.popleft()
+# custome.clear()
+
+print(custome)
