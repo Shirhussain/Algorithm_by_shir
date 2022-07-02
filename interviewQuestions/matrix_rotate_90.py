71a44c5132465daa885533b8244595627924b47f
Shir Danishyar <sh.danishyar@gmail.com>
2022-07-02 15:42:27 -0500

    rotate matrex added

---
+++ interviewQuestions/matrix_rotate_90.py
@@ -0,0 +1,24 @@
+mat = [[1, 2, 3, 4],
+       [5, 6, 7, 8],
+       [9, 10, 11, 12],
+       [13, 14, 15, 16]]
+
+
+def rotate_metrix(metrix):
+    for i in range(len(metrix)):
+        metrix[i].reverse()
+        
+    for i in range(len(metrix)):
+        for j in range(i, len(metrix)):
+            metrix[i][j] , metrix[j][i] = metrix[j][i], metrix[i][j]
+rotate_metrix(mat)
+
+
+def display_metrix(metrix):
+    for i in range(0, len(metrix)):
+        for j in range(0, len(metrix)):
+            print(metrix[i][j], end = " ")
+        print()
+
+display_metrix(mat)
+            
