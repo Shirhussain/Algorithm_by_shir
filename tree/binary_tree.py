b30de95154899a72516ea7ebd8a3ef32ef5fe3ca
Shir Danishyar <sh.danishyar@gmail.com>
2022-07-03 00:09:34 -0500

    binary three modified

--- tree/binary_tree.py
+++ tree/binary_tree.py
@@ -1,3 +1,4 @@
+from calendar import c
 import queue_linked_list as queue
 
 class TreeNode:
@@ -67,3 +68,120 @@ def level_order_traversal(rootNode):
 
 
 level_order_traversal(new_binary_tree)
+print("\n================================")
+
+
+def serach_binary_tree(rootNode, nodeValue):
+    if not rootNode:
+        return "Binary tree does not exist"
+    custome = queue.Queue()
+    custome.enqueue(rootNode)
+    while not (custome.isEmpty()):
+        root= custome.dequeue()
+        if root.value.data == nodeValue:
+            return "sucess"
+        if (root.value.leftChild is not None):
+            custome.enqueue(root.value.leftChild)
+        if (root.value.rightChild is not None):
+            custome.enqueue(root.value.rightChild)
+    return "not found"
+print(serach_binary_tree(new_binary_tree, "Coffee"))
+print("--------------------------------")
+
+
+def insert_binary_tree(rootNood, new_value):
+    if not rootNood:
+        rootNood = new_value
+    custome_Queue = queue.Queue()
+    custome_Queue.enqueue(rootNood)
+    while not custome_Queue.isEmpty():
+        root = custome_Queue.dequeue()
+        if root.value.leftChild is not None:
+            custome_Queue.enqueue(root.value.leftChild)
+        else:
+            root.value.leftChild = new_value
+            return "successfully inserted"
+        if root.value.rightChild is not None:
+            custome_Queue.enqueue(root.value.rightChild)
+        else:
+            root.value.rightChild = new_value
+            return "successfully inserted value"
+
+new_node = TreeNode("cola")
+print(insert_binary_tree(new_binary_tree, new_node))
+level_order_traversal(new_binary_tree)
+print("--------------------------------")
+
+
+def getDeepestNode(rootNode):
+    if not rootNode:
+        return
+    custome_qu = queue.Queue()
+    custome_qu.enqueue(rootNode)
+    while not custome_qu.isEmpty():
+        root = custome_qu.dequeue()
+        if root.value.leftChild is not None:
+            custome_qu.enqueue(root.value.leftChild)
+        if root.value.rightChild is not None:
+            custome_qu.enqueue(root.value.rightChild)
+    deepestNode = root.value
+    return deepestNode
+
+deepest_node = getDeepestNode(new_binary_tree)
+print(deepest_node.data)
+print("\n ----------------------------------------------------------------")
+
+
+
+def deleteDeepestNode(rootNode, deep_node):
+    if not rootNode:
+        return 
+    custome_que = queue.Queue()
+    custome_que.enqueue(rootNode)
+    while not custome_que.isEmpty():
+        root = custome_que.dequeue()
+        if root.value is deep_node:
+            root.value = None
+            return 
+        if root.value.rightChild:
+            if root.value.rightChild is deep_node:
+                root.value.rightChild = None
+                return
+            else:
+                custome_que.enqueue(root.value.rightChild)
+        if root.value.leftChild:
+            if root.value.leftChild is deep_node:
+                root.value.leftChild = None
+                return 
+            else:
+                custome_que.enqueue(root.value.leftChild)
+
+new_nod = getDeepestNode(new_binary_tree)
+deleteDeepestNode(new_binary_tree, new_nod)
+level_order_traversal(new_binary_tree)
+print("\n ----------------------------------------------------------------")
+
+
+def delete_node_BT(rootNode, data_node):
+    if not rootNode:
+        return 
+    custome_que = queue.Queue()
+    custome_que.enqueue(rootNode)
+    while not custome_que.isEmpty():
+        root = custome_que.dequeue()
+        if root.value.data == data_node:
+            deepest_node = getDeepestNode(rootNode)
+            root.value.data = deepest_node.data 
+            deleteDeepestNode(rootNode, deepest_node)
+            return "The nood has been successfully created"
+        if root.value.leftChild is not None:
+            custome_que.enqueue(root.value.leftChild)
+            
+        if root.value.rightChild is not None:
+            custome_que.enqueue(root.value.rightChild)
+    return "Failed to delete"
+
+# you can see that when I delete "Hot" then coffee will be replaced with "Hot".
+delete_node_BT(new_binary_tree, "Hot")
+level_order_traversal(new_binary_tree)
+print("\n ----------------------------------------------------------------")
