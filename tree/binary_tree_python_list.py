fa66fbf5ea98d967af34e135e1d835442721dbd5
Shir Danishyar <sh.danishyar@gmail.com>
2022-07-11 23:28:22 -0500

    binary tree python list

--- tree/binary_tree_python_list.py
+++ tree/binary_tree_python_list.py
@@ -1,3 +1,7 @@
+from hashlib import new
+from operator import index, ne
+
+
 class Binary_tree_python_list:
     def __init__(self, size):
         self.custome_list = size * [None]
@@ -24,13 +28,67 @@ class Binary_tree_python_list:
         self.pre_order_traversal(index *2)
         self.pre_order_traversal(index *2 + 1) 
         
+    def inOrderTraversal(self, index):
+        if index > self.lastUsedIndex:
+            return 
+        self.inOrderTraversal(index*2)
+        print(self.custome_list[index])
+        self.inOrderTraversal(index*2 + 1)
+        
+    def post_order_traversal(self, index):
+        if index > self.lastUsedIndex:
+            return 
+        self.post_order_traversal(index*2)
+        self.post_order_traversal(index*2 + 1)
+        print(self.custome_list[index])
+        
+    
+    def level_order_traversal(self, index):
+        for i in range(index, self.lastUsedIndex+1):
+            print(self.custome_list[i])
             
+    
+    def deleteNode(self, value):
+        if self.lastUsedIndex == 0:
+            return "there is not any node to delete"
+        for i in range(1, self.lastUsedIndex + 1):
+            if self.custome_list[i] == value:
+                self.custome_list[i] = self.custome_list[self.lastUsedIndex]
+                self.custome_list[self.lastUsedIndex] = None
+                self.lastUsedIndex -= 1
+                return "The node has been successfully deleted"
+            
+    def delete_binary_tree(self):
+        self.custome_list = None
+        return "The binary tree has been successfully deleted"
+    
 new_binary_tree = Binary_tree_python_list(8)
 print(new_binary_tree.insertNode("Drink"))
 print(new_binary_tree.insertNode("Hot"))
 print(new_binary_tree.insertNode("Cold"))
-print(new_binary_tree.search("Hot"))
+print(new_binary_tree.insertNode("Tee")) 
 print(new_binary_tree.insertNode("Coffee"))
+
+print(new_binary_tree.search("Hot"))
 
 
 new_binary_tree.pre_order_traversal(1)
+print("--------------------------------")
+
+new_binary_tree.inOrderTraversal(1)
+
+print("--------------------------------")
+new_binary_tree.post_order_traversal(1)
+
+print("--------------------------------")
+new_binary_tree.level_order_traversal(1)
+
+print("--------------------------------")
+
+print(new_binary_tree.deleteNode("Tee"))
+
+new_binary_tree.level_order_traversal(1)
+
+print("--------------------------------")
+
+print(new_binary_tree.delete_binary_tree())
