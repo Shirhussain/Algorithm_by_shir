f518dbca1fbb1163b53dc2594f0242efd2c81461
Shir Danishyar <shir.danishyar@scenera.net>
2022-11-19 15:55:41 -0800

    modified graph

--- graph/Graph.py
+++ graph/Graph.py
@@ -26,9 +26,64 @@ class Graph:
             return True
         return False
 
+    def remove_edge(self, vertex1, vertex2):
+        # doing something like this
+        # {
+        #   A: [],
+        #   B: [],
+        # }
+        # with try we try when we remove the edge which doesn't exist so we have to pass
+        # otherwise it will give us value error
+        if vertex1 in self.adjacency_list.keys() and vertex2 in self.adjacency_list.keys():
+            try:
+                self.adjacency_list[vertex1].remove(vertex2)
+                self.adjacency_list[vertex2].remove(vertex1)
+            except ValueError:
+                pass
+            return True
+        return False
+
+    def remove_vertex(self, vertex):
+        # {
+        #   A: [B,C,D],                                                  A: [B,C,],
+        #   B: [A,C],                                                    B: [A,C],
+        #   C: [A,B,D],                                                  C: [A,B,]
+        #   D: [A,C] --> consider i remove the D
+        # }
+        if vertex in self.adjacency_list.keys():
+            for other_vertex in self.adjacency_list[vertex]:
+                # here i need to see the list of edges with this vertex and then i have to remove
+                # it from other lists (other_vertex) too
+                self.adjacency_list[other_vertex].remove(vertex)
+            del self.adjacency_list[vertex]
+            return True
+
 
 new_graph = Graph()
 new_graph.add_vertex('A')
 new_graph.add_vertex('B')
 new_graph.add_edge('A', 'B')
+new_graph.print_graph()
+
+
+print("================================remove edge================================")
+new_graph.add_vertex('C')
+new_graph.add_vertex('D')
+
+new_graph.add_edge('A', 'C')
+new_graph.add_edge('B', 'C')
+new_graph.print_graph()
+new_graph.remove_edge('A', 'C')
+print("This is after remove")
+new_graph.remove_edge('A', 'D')
+new_graph.print_graph()
+
+print("================================remove vertex================================")
+
+new_graph.add_edge('A', 'C')
+new_graph.add_edge('A', 'D')
+new_graph.add_edge('C', 'D')
+new_graph.print_graph()
+print("after remove")
+new_graph.remove_vertex('D')
 new_graph.print_graph()
