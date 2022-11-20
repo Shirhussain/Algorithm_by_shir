8db81be8480f6ca00a74c1bae5adaf441df967f0
Shir Danishyar <shir.danishyar@scenera.net>
2022-11-20 12:42:13 -0800

    single source shortest path added

---
+++ graph/single_source_shourtest_path/bridth_first_search.py
@@ -0,0 +1,44 @@
+# enqueue any starting vertex
+# while queue is not empty
+#   p = dequeue()
+#   if p is unvisited:
+#       mark it as visited
+#       enqueue all adjacent unvisited vertices of p update parent of adjacent vertices to curVertex
+
+
+class Graph:
+    def __init__(self, graph_dict=None):
+        if graph_dict is None:
+            graph_dict = {}
+        self.graph_dict = graph_dict
+
+    # it only works for related graph
+
+    def breadth_first_search(self, start, end):
+        queue = []
+        queue.append([start])
+        while queue:
+            path = queue.pop(0)
+            # get the last element of the queue
+            node = path[-1]
+            if node == end:
+                return path
+            for adjacent in self.graph_dict.get(node, []):
+                new_path = list(path)
+                new_path.append(adjacent)
+                queue.append(new_path)
+
+
+custom_dict = {
+    "a": ["b", "c"],
+    "b": ["d", "g"],
+    "c": ["d", "e"],
+    "d": ["f"],
+    "e": ["f"],
+    "g": ["f"]
+}
+
+new_graph = Graph(custom_dict)
+# starting form 'a'
+print(new_graph.breadth_first_search('a', 'f'))
+print(new_graph.breadth_first_search('a', 'e'))
