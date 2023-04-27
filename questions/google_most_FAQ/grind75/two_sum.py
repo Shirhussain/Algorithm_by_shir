dceeb70921e9829146eb8d539785bb5316a6799a
Shir Danishyar <sh.danishyar@gmail.com>
2023-04-26 22:21:32 -0700

    two sume added

--- questions/google_most_FAQ/grind75/two_sum.py
+++ questions/google_most_FAQ/grind75/two_sum.py
@@ -5,4 +5,20 @@
 # Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
 def two_sum(nums, target):
-    pass
+    arr = [(num, idx) for idx, num in enumerate(nums)]
+    sorted_arr = sorted(arr, key=lambda x: x[0])
+    
+    left = 0
+    right = len(sorted_arr)-1
+    while left < right:
+        sum = sorted_arr[left][0] + sorted_arr[right][0]
+        if sum > target:
+            right -= 1
+        elif sum < target:
+            left += 1
+        else:
+            return sorted_arr[left][1], sorted_arr[right][1]
+
+nums = [2,7,11,4,15] 
+target = 15
+print(two_sum(nums, target))
