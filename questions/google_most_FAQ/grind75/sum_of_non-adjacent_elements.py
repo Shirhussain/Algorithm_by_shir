290240efa5c3e5f3834e297ca5733c88923984b4
Shir Danishyar <sh.danishyar@gmail.com>
2023-04-30 14:29:08 -0700

    Split Array Largest Sum

---
+++ questions/google_most_FAQ/grind75/sum_of_non-adjacent_elements.py
@@ -0,0 +1,14 @@
+# Given an array of positive integers, find the maximum sum of non-adjacent elements in the array. You need to write a function that returns this maximum sum.
+
+# def max_pair_sum(arr: List[int]) -> Tuple[int, int]:
+#     pass
+
+# Input: arr: A list of integers
+# Output: A tuple containing two integers
+
+# Input: arr = [5, 7,1, 3,1, 2]
+# Output: (5, 3)
+
+# Explanation:
+# The maximum sum of non-adjacent elements is obtained by selecting elements at indices 0 and 3 (5 + 5 = 10).
+
---
+++ questions/google_most_FAQ/udemy leetcode/find_maximum_of_k_consictve.py
@@ -0,0 +1,38 @@
+def kth_max_sum(arr, window_size):
+    arr_size = len(arr)
+    if arr_size < window_size:
+        print("Invalid operation the window size can't be less than arr size")
+        return -1
+    window_sum = sum(arr[i] for i in range(window_size))
+    max_sum = window_sum
+    
+    for i in range(arr_size - window_size):
+        window_sum = window_sum - arr[i] + arr[i+window_size]
+        max_sum = max(max_sum, window_sum)
+    return max_sum
+
+
+
+
+k = 2
+# arr = [80, -90, 100, -1200, 200]
+arr = [80, -50, 90, 100]
+
+print(kth_max_sum(arr, k))
+
+
+
+
+# broth force way
+def k_max_sum(arr, k):
+    max_sum = float('-inf')
+    n = len(arr)
+    for i in range(n-k+1):
+        current_sum = 0
+        for j in range(k):
+            current_sum += arr[i+ j]
+        max_sum = max(max_sum, current_sum)
+    return max_sum
+        
+
+print(k_max_sum(arr, k))
