84617914f984cec15744017e3e883f35be72ef03
Shir Danishyar <sh.danishyar@gmail.com>
2023-05-10 00:17:51 -0700

    non repeating char updated

--- questions/google_most_FAQ/grind75/non_reapeating_char.py
+++ questions/google_most_FAQ/grind75/non_reapeating_char.py
@@ -22,3 +22,21 @@ def counter(arr):
             yield num
 
 print(list(counter([1, 1, 1, 5, 2, 1, 3, 4, 2])))
+
+
+def count(arr):
+    len_arr = len(arr)
+    visited = [False for _ in range(len_arr)]
+    for i in range(len_arr):
+        if visited[i] == True:
+            continue
+        count = 1
+        for j in range(i+1, len_arr):
+            if arr[i] == arr[j]:
+                visited[j] = True
+                count += 1
+        if count == 1:
+            print(arr[i])
+            
+
+print(list(count([1, 1, 1, 5, 2, 1, 3, 4, 2])))
