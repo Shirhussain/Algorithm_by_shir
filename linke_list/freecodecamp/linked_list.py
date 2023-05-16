cc5741fa509e66a9767032963b70ed995ebfbb60
Shir Danishyar <sh.danishyar@gmail.com>
2023-05-16 14:57:23 -0700

    linked list update

---
+++ linke_list/freecodecamp/linked_list.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+        
+class LinkedList(object):
+    def __init__(self, head=None):
+        self.head = head
+        
+    def append(self, new_node):
+        current = self.head
+        if current:
+            while current.next:
+                current = current.next
+            current.next = new_node
+        else:
+            self.head = new_node
+            
+    def delete(self, value):
+        current = self.head
+        if current.value == value:
+            self.head = current.next
+        else:
+            while current:
+                if current.value == value:
+                    break
+                prev = current
+                current = current.next
+            if current is None:
+                return
+            prev.next = current.next 
+            current = None 
+            
+            
