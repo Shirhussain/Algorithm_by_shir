78986ca5ceae73279f43f9c675b39d71e84f1d1a
Shir Danishyar <sh.danishyar@gmail.com>
2023-06-07 17:52:26 -0700

    tabluation added

--- Dynamic_programming/how_many_uniq_way.py
+++ Dynamic_programming/how_many_uniq_way.py
@@ -119,3 +119,41 @@ def cashedCoinSum(total_input, coins_list):
 
 
 print(cashedCoinSum(1000, [1,2,3,4,5,6,7,8,9,10,11,12]))
+
+
+
+# tabulation coin sum
+# total(5),[coins(1,2,3)]
+"""
+based on previous question we know that if coins become empty and total become zero we return 1
+            0   1   2   3   4  5
+        -------------------------
+[]      |   1   0   0   0   0  0 
+[1]     |   1   1   1   1   1  1
+[1,2]   |   1   1   2   2   3  3
+[1,2,3] |   1   1   2   2   4  4 
+
+
+
+1, identify factor (total, coins)
+2, create table increamintely increasing factors 
+    what is smallest version ?
+    what is eventual version ?
+3, determine formula
+4, create fuction - create first row 
+5, fill out rest of the table
+
+"""
+
+
+def tabulationCoinSum(total_input, coins_input):
+    table = [0]*(total_input+1)
+    table[0] = 1
+    for coin in coins_input:
+        for i in range(coin, len(table)):
+            # print("thisis each row: -->table: ", table[i], "table[i-coins]", i-coin, "coin -> :", coin)
+            table[i] = table[i] + table[i-coin]
+    return table[total_input]
+
+print(tabulationCoinSum(10,[2,5,3,6]))
+
