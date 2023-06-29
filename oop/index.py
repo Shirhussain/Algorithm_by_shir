625f0a1d78ddf93e679b60f66a0497fd7031046a
Shir Danishyar <sh.danishyar@gmail.com>
2023-06-28 20:42:26 -0700

    oop design

--- oop/index.py
+++ oop/index.py
@@ -2,4 +2,39 @@
 composition is something that we have to ask is it depends on the next entity.
 like: elvator is dpendnt to a building it means it's a comosition fo building, 
 and we can't use elvator without building.
+
+my class at outco link for excalidraw diagram
+
+https://excalidraw.com/#room=a46ca57e07016fb92f85,bRBOW07vQs9TIppFmAgdFQ
+
+https://excalidraw.com/#room=b12604d4ea61e52892f4,StzA6eGdUsW4QlwA15xobA
+
+
+1.  What does loose coupling mean?
+    Loose coupling is an approach to interconnecting the components in a system or 
+    network so that those components, also called elements, depend on each other to
+    the least extent practicable. Coupling refers to the degree of direct knowledge 
+    that one element has of another.
+    
+    Example : If you change your shirt, then you are not forced to change your body
+    
+2. What is the meaning of high cohesion?
+    how closely all the routines in a class, or all the code in a routine, support a central purpose.
+    high cohesion means that class should do one thing and one thing very well
+
+3. How do you define single responsibility?
+    the concept that any single object in object-oriented programing (OOP)
+    should be made for one specific function
+    
+4. What is the open closed principle?
+    a design principle that states that software components (such as classes and methods) should be open
+    for extension but closed for modification
+
+
+5. What is meant by Interface Segregation Principle?
+    Clients should not be forced to depend upon interfaces that they do not use
+    
+6. What is the dependency inversion principle in simple terms?
+    high-level modules should depend on abstractions rather than concrete implementations.
+    
 """
