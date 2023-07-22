2fdd08067c9aebf31c22950d497f0f9e0d2079bb
Shir Danishyar <sh.danishyar@gmail.com>
2023-07-22 14:05:17 -0700

    print array with recursion

--- recursion/outco/pring_array_reverse.py
+++ recursion/outco/pring_array_reverse.py
@@ -9,3 +9,27 @@ def recursive(l):
 
 
 print(recursive(l))
+
+
+def print_array_recursive(arr):
+    if not arr:  # Base case: the array is empty
+        return
+    else:
+        print(arr[0])  # Print the first element (head)
+        # Recursive call with the remaining elements (tail)
+        print_array_recursive(arr[1:])
+
+
+# Example usage
+my_array = [1, 2, 3, 4, 5]
+print_array_recursive(my_array)
+
+
+def rec_array(arr):
+    if not arr:
+        return
+    rec_array(arr[1:])
+    return arr
+
+
+print(rec_array(my_array))
