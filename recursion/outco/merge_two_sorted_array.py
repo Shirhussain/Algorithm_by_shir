fb34753b27618275713b9e888396ae8a2e0729ff
Shir Danishyar <sh.danishyar@gmail.com>
2023-07-25 17:40:09 -0700

    update merg

--- recursion/outco/merge_two_sorted_array.py
+++ recursion/outco/merge_two_sorted_array.py
@@ -87,3 +87,59 @@ l2 = [0, 2, 5, 6, 8, 9]
 
 if __name__ == '__main__':
     print(merge_sorted_lists(l1, l2))
+
+
+def mrg(array1, array2):
+    i, j = 0, 0
+    result = []
+    while i < len(array1) and j < len(array2):
+        if array1[i] <= array2[j]:
+            result.append(array1[i])
+            i += 1
+        else:
+            result.append(array2[j])
+            j += 1
+    # remain = array1[i:] + array2[j:]
+    result.extend(array1[i:] + array2[j:])
+    return result
+
+
+l1 = [1, 3, 4, 7]
+l2 = [0, 2, 5, 6, 8, 9]
+
+print(mrg(l1, l2))
+
+
+def merging(l1, l2):
+    result = []
+    while len(l1) > 0 and len(l2) > 0:
+        if l1[0] > l2[0]:
+            result.append(l1.pop(0))
+        else:
+            result.append(l2.pop(0))
+    result.extend(l1 + l2)
+    return result
+
+
+print(merging(l1, l2))
+
+
+def merg_all(l1, l2):
+    result = []
+    n = len(l1) + len(l2)
+    i, j = 0, 0
+    while i + j < n:
+        if j >= len(l2) or l1[i] < l2[j] and i < len(list1):
+            result.append(l1[i])
+            i += 1
+        else:
+            result.append(l2[j])
+            j += 1
+    return result
+
+
+array1 = [1, 9, 22]
+array2 = [0, 2, 4, 6, 8, 19]
+
+
+print(merg_all(array1, array2))
