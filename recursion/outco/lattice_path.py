062a84b1a38ac132dab537aad4368eff06cd0baa
Shir Danishyar <sh.danishyar@gmail.com>
2023-08-01 19:18:20 -0700

    recusive uniqpath

--- recursion/outco/lattice_path.py
+++ recursion/outco/lattice_path.py
@@ -4,15 +4,12 @@
 # Input 1: (2, 3)
 
 #     (2 x 3 lattice of squares)
-    
-#    0 __1__2__3 
+
+#    0 __1__2__3
 #    1|__|__|__|
 #    2|__|__|__|
 
 # Output 1: 10 (number of unique paths from top left corner to bottom right corner)
-
-
-
 
 
 # Input 2: (2, 2)
@@ -32,14 +29,52 @@ def latticePaths(row, col):
         because it's very easy to find outband paths or find edge and base cases
     """
     if row < 0 or col < 0:
-        return 0 
+        return 0
     if row == 0 and col == 0:
-        return 1 
-    up = latticePaths(row -1, col)
+        return 1
+    up = latticePaths(row - 1, col)
     left = latticePaths(row, col-1)
-    return left + up 
+    return left + up
 
 
-print(latticePaths(2,2))
+print(latticePaths(2, 2))
 
 # time complexity is (2^m+n)
+
+
+# Leet code problem  my solution
+# https://leetcode.com/problems/unique-paths/solutions/2999834/python-3-solutions-video-solution/
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        if m == 0 or n == 0:
+            return 0
+        if m == 1 or n == 1:
+            return 1
+        return self.uniquePaths(m-1, n) + self.uniquePaths(m, n-1)
+
+
+s = Solution()
+print(s.uniquePaths(3, 7))
+
+
+class MemoSolution(object):
+    def uniquePaths(self, m: int, n: int) -> int:
+        memo = {}
+        if (m, n) in memo:
+            return memo[(m, n)]
+
+        if m == 0 or n == 0:
+            memo[(m, n)] = 0
+            return 0
+
+        if m == 1 or n == 1:
+            memo[(m, n)] = 1
+            return 1
+
+        memo[(m, n)] = self.uniquePaths(m-1, n) + self.uniquePaths(m, n-1)
+        return memo[(m, n)]
+
+
+memo_sol = MemoSolution()
+print(memo_sol.uniquePaths(3, 7))
