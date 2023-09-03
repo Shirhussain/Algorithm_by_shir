a05306d391ba7006969375d0e496b464c31342cf
Shir Danishyar <sh.danishyar@gmail.com>
2023-09-02 19:53:14 -0700

    add memo count construct

--- Dynamic_programming/count_constract.py
+++ Dynamic_programming/count_constract.py
@@ -25,3 +25,29 @@ def count_construct(target, word_break):
 
 
 print(count_construct('purple', ['purp', 'p', 'ur', 'le', 'purpl']))
+
+# time O(m*n^m ) Space = O(m^2)
+
+
+def count_construct_memo(target, word_break, memo={}):
+    if target in memo:
+        return memo[target]
+
+    if target == "":
+        return 1
+
+    total_count = 0
+    for word in word_break:
+        if target.startswith(word):
+            suffix = target[len(word):]
+            rest_count = count_construct_memo(suffix, word_break, memo)
+            total_count += rest_count
+    memo[target] = total_count
+    return total_count
+
+
+print(count_construct_memo('purple', ['purp', 'p', 'ur', 'le', 'purpl']))
+print(count_construct_memo("eeeeeeeeeeeeeeeeeeeeeeeeeeeee", [
+      "e", "ee", "eee", "ef"]))
+
+# Time O(n*m^2) Space O(m^2)
