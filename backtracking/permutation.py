219e7144d12e07bd79727872a6cff3e5edbcdec3
Shir Danishyar <sh.danishyar@gmail.com>
2023-09-07 00:48:35 -0700

    permeutation added

--- backtracking/permutation.py
+++ backtracking/permutation.py
@@ -17,6 +17,34 @@
 
     Input: nums = [1]
     Output: [[1]]
+    
+    
+    level one:                                                    []
+    level two:                                  [1]                [2]           [3]
+    level three:                          [1,2] [1,3]          [2,1][2,3]       [3,1][3,2]
+    level four:                 [1,2,3] [1,3,2]            [2,1,3] [2,3,1]      [3,1,2] [3,2,1]
 
 
 """
+
+
+def permutation(nums):
+    visited = set()
+    result = []
+
+    def helper(subset):
+        if len(subset) == len(nums):
+            result.append(subset)
+            return
+
+        for i in range(len(nums)):
+            if i not in visited:
+                visited.add(i)
+                helper(subset+[nums[i]])
+                visited.remove(i)
+
+    helper([])
+    return result
+
+
+print(permutation([1, 2, 3]))
