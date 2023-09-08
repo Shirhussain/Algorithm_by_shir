1e5dae92bd055d29efb39bd83f49fc028dfc5cbd
Shir Danishyar <sh.danishyar@gmail.com>
2023-09-08 14:27:28 -0700

    letter case update

--- backtracking/letterCasePermutation.py
+++ backtracking/letterCasePermutation.py
@@ -14,4 +14,39 @@ Input: s = "3z4"
 Output: ["3z4","3Z4"]
 
 
+
+                                    ""
+                                a/       A\             0
+                               1         1              1
+                            b/   B\      b/  B\         2
+                        a1b      a1B    A1b   A1B       
+                        2|        2|    2|     2|       3
+                        a1b2     a1B2   A1b2   A1B2
+
+
 """
+
+
+def letterCasePermutation(s):
+    result = []
+
+    def helper(substr, idx):
+        if idx == len(s):
+            result.append(substr)
+            return
+
+        curr_char = s[idx]
+
+        if curr_char.isalpha():
+            helper(substr+curr_char.lower(), idx+1)
+            helper(substr+curr_char.upper(), idx+1)
+        else:
+            # if it's a number
+            helper(substr+curr_char, idx+1)
+
+    helper("", 0)
+
+    return result
+
+
+print(letterCasePermutation("a1b2"))
