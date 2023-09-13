e5c880e4e871402229892b78041dd427c8cb3b57
Shir Danishyar <sh.danishyar@gmail.com>
2023-09-13 15:16:19 -0700

    longest path

--- tree/outco/longest_path_binary_tree.py
+++ tree/outco/longest_path_binary_tree.py
@@ -33,3 +33,60 @@ max_path_length(node)
   return dfs(node)
 
 '''
+
+
+class TreeNode:
+    def __init__(self, value=None):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def lst_to_tree(nums):
+    nums.sort()
+
+    def helper(lst):
+        mid = len(lst)//2
+        root = TreeNode(lst[mid])
+        root.left = TreeNode(lst[:mid])
+        root.right = TreeNode(lst[mid+1:])
+        return root
+    return helper(nums)
+
+
+lst = [1, 2, 3, 4]
+sample_tree = lst_to_tree(lst)
+
+
+def max_depth(root):
+    def dfs(node):
+        if node:
+            left_max = dfs(node.left)
+            right_max = dfs(node.right)
+            return max(left_max, right_max) + 1
+        else:
+            return 0
+    return dfs(root)
+
+
+print(max_depth(sample_tree))
+
+
+def max_depth_tree(root):
+    def helper(node):
+        return 0 if node is None else max(helper(node.left), helper(node.right)) + 1
+    return helper(root)
+
+
+print(max_depth_tree(sample_tree))
+
+
+def print_tree(root, level=0, prefix="Root: "):
+    if root is not None:
+        print(" " * (level * 4) + prefix + str(root.value))
+        if root.left is not None or root.right is not None:
+            print_tree(root.left, level + 1, "L--- ")
+            print_tree(root.right, level + 1, "R--- ")
+
+
+print_tree(sample_tree)
