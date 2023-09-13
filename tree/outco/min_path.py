bf7ebf47aff5b854aebf833c52aebd2595829b2c
Shir Danishyar <sh.danishyar@gmail.com>
2023-09-13 16:51:55 -0700

    min depth binary tree

--- tree/outco/min_path.py
+++ tree/outco/min_path.py
@@ -74,3 +74,127 @@ find_min_depth(node)
 
   return bfs(node, 1)
 '''
+
+
+
+
+import random
+from collections import deque
+class BstNode:
+
+    def __init__(self, key):
+        self.key = key
+        self.right = None
+        self.left = None
+
+    def min_depth(self):
+        if self.key is None:
+            return 0
+
+        q = deque((self,))
+        result = 0
+
+        while q:
+            result += 1
+            for _ in range(len(q)):
+                node = q.popleft()
+                if node:
+                    if node.left is None and node.right is None:
+                        return result
+                    q.append(node.left)
+                    q.append(node.right)
+        return result
+
+    def min_depth_bfs(self):
+        if self.key is None:
+            return 0
+
+        q = deque([(self, 1)])
+
+        while q:
+            node, path = q.popleft()
+            if node:
+                if node.left is None and node.right is None:
+                    return path
+                q.append((node.left, path+1))
+                q.append((node.right, path+1))
+        return 0
+
+    def insert(self, key):
+        if self.key == key:
+            return
+        elif self.key < key:
+            if self.right is None:
+                self.right = BstNode(key)
+            else:
+                self.right.insert(key)
+        else:  # self.key > key
+            if self.left is None:
+                self.left = BstNode(key)
+            else:
+                self.left.insert(key)
+
+    def display(self):
+        lines, *_ = self._display_aux()
+        for line in lines:
+            print(line)
+
+    def _display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if self.right is None and self.left is None:
+            line = '%s' % self.key
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if self.right is None:
+            lines, n, p, x = self.left._display_aux()
+            s = '%s' % self.key
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if self.left is None:
+            lines, n, p, x = self.right._display_aux()
+            s = '%s' % self.key
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left._display_aux()
+        right, m, q, y = self.right._display_aux()
+        s = '%s' % self.key
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * \
+            '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + \
+            (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + \
+            [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+
+nums = [1, 3, 9, 20, 15, 7]
+b = BstNode(3)
+# for _ in range(50):
+#     b.insert(random.randint(0, 100))
+for num in nums:
+    b.insert(num)
+b.display()
+
+print(b.min_depth())
+print(b.min_depth_bfs())
