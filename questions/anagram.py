73eb410185c2a8723499ff9e322df9f78e0f84ed
Shir Danishyar <sh.danishyar@gmail.com>
2024-01-24 18:16:37 -0800

    add shirlock anagram

--- questions/anagram.py
+++ questions/anagram.py
@@ -46,3 +46,83 @@ def anagram(s1, s2):
 
 
 print(anagram(string1, string2))
+
+
+'''
+Two strings are anagrams of each other if the letters of one string can be rearranged to form the other string. Given a string, find the number of pairs of substrings of the string that are anagrams of each other.
+
+Example: s = mom
+
+The list of anagrammatic pairs is [m,m], [mo, om], so output is 2.
+
+Example: s = abba, output is 4: [a,a],[ab,ba],[abb,bba],[b,b]
+
+Example: s = abcd, output is 0
+
+Example: s = abac, output is 2 --> [a,a], [ab, ba]
+             i  j
+             l=1: a, a +1             i[a] = 1, j[a] = 1
+             l=2: ab, ac              i[a]-1, i[b]=1, i[c]=0  j[a]=1 j[b]=0 j[c]=1
+            
+             l=1: a, b
+             l=2: ab, ba  +1
+             l=3: aba, bac
+
+Example: s = kkkk, output is 10 [[0,1],[1,2], [0,1],[2,3], ...]
+                                   k     k       k    k 
+
+Constraint: 2 <= |s| <= 100
+
+abba
+
+
+def 
+
+'''
+
+
+print("#####\n")
+
+
+def sherlock_and_anagrams(s):
+    def count_anagrams(current_index, arr):
+        current_element = arr[current_index]
+        arr_rest = arr[current_index + 1:]
+        counter = 0
+
+        for i in range(len(arr_rest)):
+            if len(current_element) == len(arr_rest[i]) and is_anagram(current_element, arr_rest[i]):
+                counter += 1
+
+        return counter
+
+    def get_all_substrings(input_str):
+        substrings = []
+        for i in range(len(input_str)):
+            for j in range(i + 1, len(input_str) + 1):
+                substrings.append(input_str[i:j])
+        return substrings
+
+    def is_anagram(str1, str2):
+        return sorted(str1) == sorted(str2)
+
+    duplicates_count = len([v for i, v in enumerate(s) if s.index(v) != i])
+
+    if not duplicates_count:
+        return 0
+
+    anagrams_count = 0
+    substrings_arr = get_all_substrings(s)
+
+    for i in range(len(substrings_arr)):
+        anagrams_count += count_anagrams(i, substrings_arr)
+
+    return anagrams_count
+
+
+# Example usage:
+s = "abba"
+result = sherlock_and_anagrams(s)
+print(result)
+
+print(sherlock_and_anagrams("mom"))
