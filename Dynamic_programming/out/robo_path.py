f5bfa03ed17d621f621a22d4a59c5cd3e272af2d
Shir Danishyar <sh.danishyar@gmail.com>
2024-02-08 23:59:17 -0800

    uniqe path with dp and tree solved

--- Dynamic_programming/out/robo_path.py
+++ Dynamic_programming/out/robo_path.py
@@ -1,5 +1,87 @@
-# in matrix of m X n how to go from (0,0) to (m,n)
-# you can only go right and down
+# 62. Unique Paths
+# There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+# Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+# The test cases are generated so that the answer will be less than or equal to 2 * 109.
 
 
-# recusive case
+# Example 1:
+
+
+# Input: m = 3, n = 7
+# Output: 28
+# Example 2:
+
+# Input: m = 3, n = 2
+# Output: 3
+# Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+# 1. Right -> Down -> Down
+# 2. Down -> Down -> Right
+# 3. Down -> Right -> Down
+
+
+def unique_path(m, n):
+    # dp = []
+    # for i in range(m):
+    #     col = []
+    #     for j in range(n):
+    #         col.append(0)
+    #     dp.append(col)
+
+    dp = [[0 for _ in range(n)] for _ in range(m)]
+
+    for i in range(m):
+        for j in range(n):
+            if i == 0 and j == 0:
+                dp[i][j] = 1
+            else:
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+    return dp[m-1][n-1]
+
+
+print(unique_path(3, 7))
+
+
+# def unique_path_tree(m, n):
+#     # [
+#     # [0, 0, 0, 0, 0, 0, 0],
+#     # [0, 0, 0, 0, 0, 0, 0],
+#     # [0, 0, 0, 0, 0, 0, 0]
+#     # ]
+
+#     #             (0,0)
+#     #         /          \
+#     #        (1,0)         (0,1)
+#     #     /    \        /    \
+#     # (2, 0)    (1,1) (1, 1)  (0, 2)
+
+#     if m < 0 or n < 0:
+#         return
+#     if m == 0 and n == 0:
+#         return 1
+#     return unique_path_tree(m-1, n) + unique_path_tree(m, n-1)
+
+
+# print(unique_path_tree(3, 7))
+
+
+def unique_path_tree(m, n):
+    memo = {}
+
+    def find_path(row, col):
+        if (row, col) in memo:
+            return memo[(row, col)]
+
+        if row == 0 or col == 0:
+            return 0
+        if col == 1 and col == 1:
+            return 1
+        memo[(row, col)] = find_path(row-1, col) + find_path(row, col-1)
+        return memo[(row, col)]
+    return find_path(m-1, n-1)
+
+
+# Example usage
+result = unique_path_tree(3, 7)
+print(result)
