86222d25501e154fe0c56ca52e24b9779e959b3f
Shir Danishyar <sh.danishyar@gmail.com>
2024-04-28 23:59:34 -0700

    add shortest path

--- graph/out/shortest_path/snake_and_lader_problem.py
+++ graph/out/shortest_path/snake_and_lader_problem.py
@@ -10,3 +10,35 @@
 # If the player reaches a cell which is the base of a ladder,
 # the player has to climb up that ladder and if reaches a cell is the mouth of
 # the snake, and has to go down to the tail of the snake without a dice throw.
+
+# the est way is to solve this kind of problem is to first try to create a graph then put this in shortest path template:
+# put that graph here:
+
+from collections import deque
+
+
+def shortest_path(graph, start, end):
+    visited = set()
+    q = deque
+    parent_dic = {}
+    parent_dic["start"] = "Null"
+
+    while q:
+        curr_node = q.popleft()
+        if curr_node not in visited:
+            # if you have many answer in your answer and you want example between 1= A,B,D,H and 2 = A,C,F,H
+            # if both of them are the answer and the question is to output in the last one in alphabetical order
+            # so only you need is sort the children here: sorted(graph[curr_node])
+            for node in graph[curr_node]:
+                if node not in visited:
+                    q.append(node)
+                    if node not in parent_dic:
+                        parent_dic[node] = curr_node
+            visited.add(curr_node)
+
+    path = []
+    path.append(end)
+    while parent_dic[end] != "Null":
+        path.append(parent_dic[end])
+        end = parent_dic[end]
+    return reversed(path)
