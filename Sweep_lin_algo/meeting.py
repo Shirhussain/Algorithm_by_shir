0cdb119f9f61fa776844dcee75b68605b354b983
Shir Danishyar <sh.danishyar@gmail.com>
2024-09-13 20:16:40 -0700

    meeting

---
+++ Sweep_lin_algo/meeting.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        if not intervals:
+            return []
+
+        intervals.sort(key=lambda x: x[0])
+        result = []
+
+        for interval in intervals:
+            if not result or result[-1][1] < interval[0]:
+                result.append(interval)
+            else:
+                result[-1][1] = max(interval[1], result[-1][1])
+        return result
