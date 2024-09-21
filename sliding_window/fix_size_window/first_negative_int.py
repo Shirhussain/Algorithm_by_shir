887758f15dff53533647299e454adfe14cc65a83
Shir Danishyar <sh.danishyar@gmail.com>
2024-09-21 14:53:33 -0700

    first negative integer

---
+++ sliding_window/fix_size_window/first_negative_int.py
@@ -0,0 +1,14 @@
+""" 
+Input : arr[] = {-8, 2, 3, -6, 10}, k = 2
+Output : -8 0 -6 -6
+First negative integer for each window of size k
+{-8, 2} = -8
+{2, 3} = 0 (does not contain a negative integer)
+{3, -6} = -6
+{-6, 10} = -6
+Input : arr[] = {12, -1, -7, 8, -15, 30, 16, 28} , k = 3
+Output : -1 -1 -7 -15 -15 0
+
+"""
+arr = [-8, 2, 3, -6, 10]
+k = 2
--- sliding_window/fix_size_window/maximum_sum.py
+++ sliding_window/fix_size_window/maximum_sum.py
@@ -1,5 +1,47 @@
+# brute force approach
+
+arr = [1, 4, 2, 10, 2, 3, 1, 0, 20]
+k = 4
+
+
+def max_sum_brute_force(arr, k):
+    if len(arr) < k:
+        return -1
+
+    n = len(arr)
+
+    global_sum = float('-inf')
+
+    for i in range(n-k+1):
+        current_sum = 0
+        for j in range(k):
+            current_sum += arr[i+j]
+        global_sum = max(global_sum, current_sum)
+    return global_sum
+
+
+print(max_sum_brute_force(arr, k))
+
+
 nums = [5, 7, 2, 8, 10, 3]
 window_size = 3
+
+
+def optimize_max_sum(arr, k):
+    if len(arr) < k:
+        return -1
+
+    window_sum = sum(arr[:k])
+
+    global_sum = window_sum
+
+    for i in range(k, len(arr)):
+        window_sum = window_sum + arr[i] - arr[i-k]
+        global_sum = max(global_sum, window_sum)
+    return global_sum
+
+
+print(optimize_max_sum(arr, k))
 
 
 def maximum_sum(nums, window_size):
