1f9ce7f6adb46f0421b65363e4f38503ac2ba4fc
Shir Danishyar <sh.danishyar@gmail.com>
2025-04-02 23:13:56 -0700

    add pattern

---
+++ matrix/patern/patterns.py
@@ -0,0 +1,142 @@
+def pattern1(n):
+    j = 1
+    for i in range(n):
+        print("* " * j)
+        j += 1
+    print()
+
+    # or
+
+    # just image all of them as a matrix row and col
+
+    for row in range(n):
+        for col in range(row + 1):
+            print("* ", end="")
+        print()
+    return
+
+
+def pattern2(n):
+    for i in range(n):
+        for j in range(n):
+            print("* ", end="")
+        print()
+    return
+
+
+def pattern3(n):
+    for i in range(n):
+        for j in range(n - i):
+            print("* ", end="")
+        print()
+    return
+
+
+def pattern4(n):
+    for i in range(1, n+2):
+        for j in range(1, i):
+            print(j, end=" ")
+        print()
+    return
+
+
+def pattern5(n):
+    for i in range(2*n):
+        if i <= n:
+            for j in range(i):
+                print("* ", end="")
+        else:
+            for j in range(2*n-i):
+                print("* ", end="")
+        print()
+    return
+
+
+def pattern6(n):
+    for i in range(n):
+        print(" "*(n-i)+"*"*i, end="")
+        print()
+
+    # or
+
+    for i in range(n):
+        for j in range(n-i):
+            print(" ", end="")
+        for j in range(i):
+            print("*", end="")
+        print()
+    return
+
+
+def pattern7(n):
+    for i in range(n):
+        for j in range(i):
+            print(" ", end="")
+        for j in range(n-i):
+            print("*", end="")
+        print()
+    return
+
+
+def pattern8(n):
+    for i in range(n):
+        for j in range(n-i):
+            print(" ", end="")
+        for j in range(i):
+            print("*", end="")
+        for j in range(i-1):
+            # to know how does it work just change (* to # ) then you will see the pattern
+            print("*", end="")
+        print()
+    return
+
+
+def pattern9(n):
+    for i in range(n):
+        for j in range(i):
+            print(" ", end="")
+        for j in range(n-i):
+            print("*", end="")
+        for j in range(n-i-1):
+            print("*", end="")
+        print()
+    return
+
+
+def pattern10(n):
+    for i in range(n):
+        # for j in range(n-i):
+        #     print(" ", end="")
+        # for j in range(i):
+        #     print("*", end=" ")
+        # print()
+        # or
+        for j in range(n-i):
+            print(" ", end="")
+        for j in range(i):
+            print("*", end=" ")
+        for j in range(i):
+            print(" ", end="")
+        print()
+    return
+
+
+def pattern11(n):
+    for i in range(n):
+        for j in range(i):
+            print(" ", end="")
+        for j in range(n-i):
+            print("*", end=" ")
+        for j in range(i):
+            print(" ", end="")
+        print()
+    return
+
+
+def pattern12(n):
+    pattern11(n)
+    pattern10(n)
+    return
+
+
+pattern12(5)
